d74083d0527a3b62733d78ba1b78b0c43f353f09
Michael Woods <mail@michaelwoods.us>
2018-09-03 08:47:01 +0000

    now with autostart script

--- web/lights_api_launcher.sh
+++ web/lights_api_launcher.sh
@@ -4,5 +4,8 @@ if pgrep -u root -x "python" > /dev/null
 then
 	echo "Running"
 else
-	sudo FLASK_APP=main.py python -m flask run --host=0.0.0.0
+	echo "Failed to find webserver"
+	echo "Trying to restart"
+	echo date
+	sudo FLASK_APP=/home/pi/raspi_neopixel_lights/web/main.py python -m flask run --host=0.0.0.0 &>> /home/pi/raspi_neopixel_lights/web/flask_log &
 fi
